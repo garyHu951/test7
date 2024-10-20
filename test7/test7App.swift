a0d96752edc739e46029d6d83371e17f56e70e86
gary <garyhu17@gmail.com>
2024-10-20 21:29:35 +0800

    Initial Commit

---
+++ test7/test7App.swift
@@ -0,0 +1,17 @@
+//
+//  test7App.swift
+//  test7
+//
+//  Created by user07 on 2024/10/20.
+//
+
+import SwiftUI
+
+@main
+struct test7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
